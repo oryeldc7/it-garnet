abcbc1aedd6f1e3599faf3d4220069ddd10e7076
oryeldc7 <145708909+oryeldc7@users.noreply.github.com>
2024-03-05 13:03:04 +0000

    images

--- state-park-project/assets/script/contact.js
+++ state-park-project/assets/script/contact.js
@@ -1,1 +1,36 @@
-<div id="wufoo-z1r0e2w0mu7c55"> Fill out my <a href="https://oryeldc7.wufoo.com/forms/z1r0e2w0mu7c55">online form</a>. </div> <script type="text/javascript"> var z1r0e2w0mu7c55; (function(d, t) { var s = d.createElement(t), options = { 'userName':'oryeldc7', 'formHash':'z1r0e2w0mu7c55', 'autoResize':true, 'height':'577', 'async':true, 'host':'wufoo.com', 'header':'show', 'ssl':true }; s.src = ('https:' == d.location.protocol ?'https://':'http://') + 'secure.wufoo.com/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() { var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; try { z1r0e2w0mu7c55 = new WufooForm(); z1r0e2w0mu7c55.initialize(options); z1r0e2w0mu7c55.display(); } catch (e) { } }; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })(document, 'script'); </script>
+<div id="wufoo-z1r0e2w0mu7c55">
+    Fill out my <a href="https://oryeldc7.wufoo.com/forms/z1r0e2w0mu7c55">online form</a>.
+</div>
+
+<script type="text/javascript">
+    var z1r0e2w0mu7c55;
+    (function(d, t) {
+        var s = d.createElement(t);
+        var options = {
+            'userName':'oryeldc7',
+            'formHash':'z1r0e2w0mu7c55',
+            'autoResize':true,
+            'height':'577',
+            'async':true,
+            'host':'wufoo.com',
+            'header':'show',
+            'ssl':true
+        };
+
+        s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'secure.wufoo.com/scripts/embed/form.js';
+
+        s.onload = s.onreadystatechange = function() {
+            var rs = this.readyState;
+            if (rs && rs != 'complete' && rs != 'loaded') return;
+            try {
+                z1r0e2w0mu7c55 = new WufooForm();
+                z1r0e2w0mu7c55.initialize(options);
+                z1r0e2w0mu7c55.display();
+            } catch (e) {}
+        };
+
+        var scr = d.getElementsByTagName(t)[0];
+        var par = scr.parentNode;
+        par.insertBefore(s, scr);
+    })(document, 'script');
+</script>
